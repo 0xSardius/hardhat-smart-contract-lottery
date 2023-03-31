c1240b65042b0ed49dbbab9e587516945b8065c5
Sardius <0xsardius@gmail.com>
2023-03-31 08:14:27 -0400

    Mock updates, added VRF2Coordinator Mock

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -1,5 +1,8 @@
 const developmentChains = require("../helper-hardhat-config").developmentChains;
 
+const BASE_FEE = ethers.utils.parseEther("0.25") //0.25 is the premium. It costs 0.25 per request
+const GAS_PRICE_LINK = 
+
 module.exports = async function ({ getNamedAccounts, deployments }) {
   const { deploy, log } = deployments;
   const { deployer } = await getNamedAccounts();
@@ -8,5 +11,10 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
   if (developmentChains.includes(network.name)) {
     log("Local network detected! Deploying mocks...");
     // deploy a mock vrf coordinator
+    await deploy("VRFCoordinatorV2Mock", {
+        from: deployer,
+        log: true,
+        args: [],
+        ") 
   }
 };
