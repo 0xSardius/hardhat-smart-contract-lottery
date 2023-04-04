0a9d36a5b8cbe64056666fa7e372ce64459ec9a7
Sardius <0xsardius@gmail.com>
2023-04-04 06:06:01 -0400

    Small adjustments to admin files

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -5,7 +5,7 @@ const networkConfig = {
     entranceFee: ethers.utils.parseEther("0.01"),
     gasLane:
       "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
-    subscriptionId: "11109",
+    subscriptionId: "xxxxx", // add in Chainlink VRF subscription ID here
     callbackGasLimit: "500000",
     interval: "30",
   },
