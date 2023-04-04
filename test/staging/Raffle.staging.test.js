92ed86954f3dce5b0b4d211c8e97cb8938a8315e
Sardius <0xsardius@gmail.com>
2023-04-04 06:05:08 -0400

    Finalized the project! Chainlin VRF up and running

--- test/staging/Raffle.staging.test.js
+++ test/staging/Raffle.staging.test.js
@@ -26,14 +26,25 @@ developmentChains.includes(network.name)
         it("Works with live Chainlink keepers and Chainlink VRF, we get a random winner", async function () {
           // enter the raffle
           const startingTimeStamp = await raffle.getLatestTimeStamp();
-          const deployerAccount = await ethers.getSigner(deployer); 
+          const deployerAccount = await ethers.getSigner(deployer);
 
           await new Promise(async (resolve, reject) => {
             raffle.once("WinnerPicked", async () => {
               try {
                 const recentWinner = await raffle.getRecentWinner();
                 const raffleState = await raffle.getRaffleState();
-                const winnerBalance = await 
+                const winnerBalance = await accounts[0].getBalance();
+                const endingTimeStamp = await raffle.getLatestTimeStamp();
+
+                await expect(raffle.getPlayer(0)).to.be.reverted;
+                assert.equal(recentWinner.toString(), accounts[0].address);
+                assert.equal(raffleState.toString(), "0");
+                assert.equal(
+                  winnerEndingBalance.toString(),
+                  winnerStartingBalance.add(raffleEntranceFee).toString()
+                );
+                assert(endingTimeStamp > startingTimeStamp);
+                resolve();
               } catch (error) {
                 console.log(error);
                 reject(e);
@@ -41,9 +52,9 @@ developmentChains.includes(network.name)
             });
 
             await raffle.enterRaffle({ value: raffleEntranceFee });
+            const winnerStartingBalance = await accounts[0].getBalance();
 
             // This code won't complete until our listener has finished listening
-
           });
         });
       });
